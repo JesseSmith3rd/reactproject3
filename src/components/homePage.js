10459dd97a3abc1943011657fb9087ee97d75bed
Jesse Smith <JesseSmith3rd@outlook.com>
2017-09-17 12:51:46 -0400

    modified homePage.js, removed additional divs

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -5,11 +5,9 @@ var React = require('react');
 var Home = React.createClass({
   render: function() {
     return (
-      <div id="app">
       <div className="jumbotron">
         <h1>Jesses Smith Demonstration</h1>
         <p>React, React Router, and Flux for duper ultra lightning fast, warp nine website.</p>
-      </div>
       </div>
     );
   }
