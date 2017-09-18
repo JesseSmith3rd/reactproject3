ccae540ee4c2e6d32d4360cf3c875b98988d87ab
Jesse Smith <JesseSmith3rd@outlook.com>
2017-09-18 08:24:44 -0400

    modified homepage.js, made update to header

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -6,7 +6,7 @@ var Home = React.createClass({
   render: function() {
     return (
         <div className="jumbotron">
-          <h1>Jesses Smith Demonstration</h1>
+          <h1>Jesse Smith React Demonstration</h1>
           <p>React, React Router, and Flux for ultra lightning fast, warp nine, engage website.</p>
         </div>
     );
